287f7c1b0afe1ccfab1595253c5964c63e1c08ff
gan.jin <jinganix@gmail.com>
2023-07-20 08:59:04 +0800

    build(service): import jacoco and checkstyle (#2)

--- service/src/main/java/demo/ddz/DdzApplication.java
+++ service/src/main/java/demo/ddz/DdzApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DdzApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DdzApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DdzApplication.class, args);
+  }
 }
--- service/src/test/java/demo/ddz/DdzApplicationTests.java
+++ service/src/test/java/demo/ddz/DdzApplicationTests.java
@@ -1,15 +1,26 @@
 package demo.ddz;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mockStatic;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @DisplayName("DdzApplication")
 class DdzApplicationTests {
 
-    @Test
-    @DisplayName("contextLoads")
-    void contextLoads() {
+  @Test
+  @DisplayName("contextLoads")
+  void contextLoads() {
+    assertDoesNotThrow(DdzApplication::new);
+    try (MockedStatic<SpringApplication> application = mockStatic(SpringApplication.class)) {
+      assertNotNull(application);
+      DdzApplication.main(null);
     }
+  }
 }
